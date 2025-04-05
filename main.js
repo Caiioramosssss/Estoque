8f75cac79e183adb54606f1ef54ab1cabb29fc78
Caiioramosssss <caiioramosssss@gmail.com>
2025-04-05 20:36:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("root").innerHTML = `
+  <h1 style="font-family: sans-serif; color: #333;">Sistema de Estoque de Puffs</h1>
+  <p>Este é seu app PWA básico em funcionamento!</p>
+  <ul>
+    <li>Cadastro de produtos</li>
+    <li>Entrada e saída de estoque</li>
+    <li>Visualização em tempo real</li>
+  </ul>
+`;
